34c6d86cb19016c72d8e1b2ce92ae001076cf27d
Slaven <slaven91@gmail.com>
2024-04-03 23:16:45 +0200

    Work bench draft

--- src/webview/src/scenes/in-game/workbench/Workbench.vue
+++ src/webview/src/scenes/in-game/workbench/Workbench.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import BackButtons from "@/components/buttons/BackButtons.vue";
+import DarkBackground from "@/components/DarkBackground.vue";
+import CraftingView from "@/scenes/in-game/workbench/CraftingView.vue";
+import GearIcon from "../../../../public/assets/workbench/GearIcon.vue";
+import WrenchIcon from "../../../../public/assets/workbench/WrenchIcon.vue";
 
 enum View {
   CRAFTING,
@@ -15,24 +20,51 @@ const view = ref<View>(View.CRAFTING);
 </script>
 
 <template>
-  <div class="relative flex h-full w-full flex-col justify-between px-20 py-11">
+  <div class="relative flex h-full w-full flex-col px-20 py-11">
     <DarkBackground />
     <div class="mx-auto flex w-full items-center justify-between">
-      <div class="w-40"><v-img :src="`./assets/vehicles/logo.png`" /></div>
+      <div class="w-40">
+        <v-img :src="`./assets/workbench/logo.svg`" />
+      </div>
       <div
-        class="no-scrollbar mx-20 flex gap-2.5 overflow-x-auto whitespace-nowrap text-base font-bold uppercase text-white"
+        class="no-scrollbar mx-20 flex gap-2.5 overflow-x-auto whitespace-nowrap font-bold uppercase text-white"
       >
         <button
           v-for="[link, label] in links"
           :key="link"
           @click="view = link"
-          class="rounded-md border border-solid border-white/30 px-11 py-3 uppercase"
+          class="h-[170px] w-[140px] px-11 py-3 text-lg font-extrabold uppercase"
           :class="{
-            'bg-sunriseYellow text-black shadow-sunriseYellow': view === link,
-            'transition duration-200 hover:bg-white/10': view !== link,
+            'text-white': view === link,
+            'text-grey-500 transition duration-200': view !== link,
           }"
         >
-          {{ label }}
+          <div class="flex flex-col items-center justify-center">
+            <img
+              v-if="view === link"
+              :src="`./assets/workbench/rectangle.svg`"
+              class="absolute"
+            />
+            <WrenchIcon
+              v-if="link === View.CRAFTING"
+              :active="view === link"
+              class="mb-4"
+            />
+            <GearIcon
+              v-if="link === View.UPGRADES"
+              :active="view === link"
+              class="mb-4"
+            />
+            <span class="pb-2">
+              {{ label }}
+            </span>
+            <div class="h-[12px] w-[74px]">
+              <img
+                v-if="view === link"
+                :src="`./assets/workbench/ornament.svg`"
+              />
+            </div>
+          </div>
         </button>
       </div>
       <BackButtons />
