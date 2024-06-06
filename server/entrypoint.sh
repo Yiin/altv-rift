c758fc1a93a4ee2dc7234dfd8d12c001f9c3c052
Yiin <stanislovas.janonis@gmail.com>
2024-06-06 08:41:57 +0300

    upd

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -19,7 +19,24 @@ case "$1" in
     bun run dev
     ;;
   server)
+    # MODULES_DIR="modules"
+    # BACKUP_DIR="/tmp/modules"
+
+    # mkdir -p "$MODULES_DIR"
+    # mkdir -p "$BACKUP_DIR"
+
+    # if [ -d "$MODULE_DIR" ] && [ "$(ls -A $MODULE_DIR)" ]; then
+    #   echo "Saving"
+    #   rsync -av --delete "$MODULE_DIR/" "$BACKUP_DIR/"
+    # fi
+
     bunx altv-pkg $ALTV_BRANCH
+
+    # if [ -d "$BACKUP_DIR" ] && [ "$(ls -A $BACKUP_DIR)" ]; then
+    #   echo "Loading"
+    #   rsync -av --delete "$BACKUP_DIR/" "$MODULE_DIR/"
+    # fi
+
     chmod +x altv-server altv-crash-handler
     bun run /root/setup/server.js
     bun run server
