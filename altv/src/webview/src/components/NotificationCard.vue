bfc47929f6fa64724c2d9976c9eefe78f41d6f13
Laurynas Puodžius <laurynaslavre@gmail.com>
2024-07-02 21:17:10 +0300

    proper use of props

--- altv/src/webview/src/components/NotificationCard.vue
+++ altv/src/webview/src/components/NotificationCard.vue
@@ -14,20 +14,20 @@ const props = defineProps<{
     <div
       class="flex items-center gap-4 p-4 rounded-lg"
       :class="{
-        'bg-red-900/10': notification.type === 'error',
-        'bg-green-900/10': notification.type === 'success',
-        'bg-blue-900/10': notification.type === 'info',
-        'bg-yellow-900/10': notification.type === 'warning'
+        'bg-red-900/10': props.notification.type === 'error',
+        'bg-green-900/10': props.notification.type === 'success',
+        'bg-blue-900/10': props.notification.type === 'info',
+        'bg-yellow-900/10': props.notification.type === 'warning'
       }"
     >
       <div class="shrink-0">
         <div
           class="flex items-center justify-center text-xs border rounded w-14 h-14"
           :class="{
-            'border-red-500': notification.type === 'error',
-            'border-green-500': notification.type === 'success',
-            'border-blue-500': notification.type === 'info',
-            'border-yellow-500': notification.type === 'warning'
+            'border-red-500': props.notification.type === 'error',
+            'border-green-500': props.notification.type === 'success',
+            'border-blue-500': props.notification.type === 'info',
+            'border-yellow-500': props.notification.type === 'warning'
           }"
         >
           icon
@@ -38,16 +38,16 @@ const props = defineProps<{
           <div
             class="inline-flex p-1 text-xs font-semibold leading-none uppercase rounded"
             :class="{
-              'bg-red-500': notification.type === 'error',
-              'bg-green-500': notification.type === 'success',
-              'bg-blue-500': notification.type === 'info',
-              'bg-yellow-500': notification.type === 'warning'
+              'bg-red-500': props.notification.type === 'error',
+              'bg-green-500': props.notification.type === 'success',
+              'bg-blue-500': props.notification.type === 'info',
+              'bg-yellow-500': props.notification.type === 'warning'
             }"
           >
-            {{ notification.type }}
+            {{ props.notification.type }}
           </div>
         </div>
-        <div class="text-lg font-bold leading-tight">{{ notification.text }}</div>
+        <div class="text-lg font-bold leading-tight">{{ props.notification.text }}</div>
       </div>
     </div>
   </div>
