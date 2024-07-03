ef63571464a0e443916379486237b6907edb70e8
Yiin <stanislovas.janonis@gmail.com>
2024-07-03 17:58:20 +0300

    fix: use shared NotificationType

--- altv/src/webview/src/components/NotificationCard.vue
+++ altv/src/webview/src/components/NotificationCard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import type { NotificationSchema } from '../notiwind'
-import { NotificationTypes } from '../notiwind'
+import type { Notification } from '../plugins/notiwind'
+import { NotificationType } from '../plugins/notiwind'
 
 const props = defineProps<{
-  notification: NotificationSchema
+  notification: Notification
 }>()
 </script>
 
@@ -15,20 +15,20 @@ const props = defineProps<{
     <div
       class="flex items-center gap-4 p-4 rounded-lg"
       :class="{
-        'bg-red-900/10': props.notification.type === NotificationTypes.Error,
-        'bg-green-900/10': props.notification.type === NotificationTypes.Success,
-        'bg-blue-900/10': props.notification.type === NotificationTypes.Info,
-        'bg-yellow-900/10': props.notification.type === NotificationTypes.Warning
+        'bg-red-900/10': props.notification.type === NotificationType.Error,
+        'bg-green-900/10': props.notification.type === NotificationType.Success,
+        'bg-blue-900/10': props.notification.type === NotificationType.Info,
+        'bg-yellow-900/10': props.notification.type === NotificationType.Warning
       }"
     >
       <div class="shrink-0">
         <div
           class="flex items-center justify-center text-xs border rounded w-14 h-14"
           :class="{
-            'border-red-500': props.notification.type === NotificationTypes.Error,
-            'border-green-500': props.notification.type === NotificationTypes.Success,
-            'border-blue-500': props.notification.type === NotificationTypes.Info,
-            'border-yellow-500': props.notification.type === NotificationTypes.Warning
+            'border-red-500': props.notification.type === NotificationType.Error,
+            'border-green-500': props.notification.type === NotificationType.Success,
+            'border-blue-500': props.notification.type === NotificationType.Info,
+            'border-yellow-500': props.notification.type === NotificationType.Warning
           }"
         >
           icon
@@ -39,10 +39,10 @@ const props = defineProps<{
           <div
             class="inline-flex p-1 text-xs font-semibold leading-none uppercase rounded"
             :class="{
-              'bg-red-500': props.notification.type === NotificationTypes.Error,
-              'bg-green-500': props.notification.type === NotificationTypes.Success,
-              'bg-blue-500': props.notification.type === NotificationTypes.Info,
-              'bg-yellow-500': props.notification.type === NotificationTypes.Warning
+              'bg-red-500': props.notification.type === NotificationType.Error,
+              'bg-green-500': props.notification.type === NotificationType.Success,
+              'bg-blue-500': props.notification.type === NotificationType.Info,
+              'bg-yellow-500': props.notification.type === NotificationType.Warning
             }"
           >
             {{ props.notification.type }}
